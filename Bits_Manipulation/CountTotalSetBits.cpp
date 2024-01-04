0adb3a50fba0ff0223d65846a3aa9bae4726e816
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-01-04 21:00:21 +0530

    Update CountTotalSetBits.cpp

--- Bits_Manipulation/CountTotalSetBits.cpp
+++ Bits_Manipulation/CountTotalSetBits.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int countSetBits(int n){
+    int c=0;
+        while(n!=0){
+            int rb=n&-n;
+            n=n-rb ;
+            c++;
+        }return c;
+}
+
+int main() {
+    int sum=0;
+        int n;
+        cin >> n;
+        for(int i=1; i<=n ; i++){
+            sum+=countSetBits(i);
+        }
+        cout << sum << endl;
+}
 
