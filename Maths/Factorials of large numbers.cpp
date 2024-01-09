291b3d72cb871da1d6d14a52c6a035a33257e7e8
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-01-09 17:51:42 +0530

    Create Factorials of large numbers.cpp

---
+++ Maths/Factorials of large numbers.cpp
@@ -0,0 +1,20 @@
+vector<int> factorial(int N){
+        // code here
+        vector<int> result;
+        result.push_back(1);
+
+        int carry = 0;
+        for (int i = 2; i <= N; ++i) {
+            for (int j = 0; j < result.size() || carry; ++j) {
+                if (j == result.size())
+                    result.push_back(0);
+
+                long long product = result[j] * i + carry;
+                result[j] = product % 10;
+                carry = product / 10;
+            }
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
